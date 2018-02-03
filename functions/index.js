b90604394b9aef90f1655acaf1e39261653a7836
Jose Zambudio Bernabeu <zamberjo@gmail.com>
2018-02-03 12:09:46 +0100

    [ADD] Intentents do, read

--- functions/index.js
+++ functions/index.js
@@ -7,16 +7,16 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-const know = admin.database().ref('/faba-counter');
-const playersRef = know.child('players');
+const know = admin.database().ref('/tortolapp-spreads');
+const spreadsRef = know.child('spreads');
 
 // Dialogflow Intent names
-const ADD_FABA_INTENT = 'add-faba'
-const COUNT_FABA_INTENT = 'count-faba'
-const REMOVE_FABA_INTENT = 'remove-faba'
+const SPREAD_DO_INTENT = 'spread-do'
+const SPREADS_READ_INTENT = 'spread-read'
 
 // Context Parameters
-const PLAYER_NAME_PARAM = 'faba_player';
+const MESSAGE_PARAM = 'message';
+const USERNAME_PARAM = 'username';
 
 exports.tortolapp = functions.https.onRequest((request, response) => {
    console.log('headers: ' + JSON.stringify(request.headers));
@@ -25,49 +25,35 @@ exports.tortolapp = functions.https.onRequest((request, response) => {
    const assistant = new Assistant({request: request, response: response});
 
    let actionMap = new Map();
-   actionMap.set(ADD_FABA_INTENT, addFaba);
-   actionMap.set(COUNT_FABA_INTENT, countFaba);
-   actionMap.set(REMOVE_FABA_INTENT, removeFaba);
+   actionMap.set(SPREAD_DO_INTENT, doSpread);
+   actionMap.set(SPREADS_READ_INTENT, readSpread);
    assistant.handleRequest(actionMap);
 
-   function addFaba(assistant) {
-        console.log('addFaba');
-        const playerName = assistant.getArgument(PLAYER_NAME_PARAM);
+   function doSpread(assistant) {
+        console.log('doSpread');
+        var userName = assistant.getArgument(USERNAME_PARAM);
+        var message = assistant.getArgument(MESSAGE_PARAM);
 
-        playersRef.child(playerName).once('value', snap => {
-            var count = (snap.val() || {}).count || 0
-
-            playersRef.child(playerName).set({
-                count: count + 1
-            });
-
-            //const speech = `<speak>Faba añadida a ${playerName}</speak>`;
-            //assistant.ask(speech);
-        });
-   }
-
-   function countFaba(assistant) {
-        console.log('countFaba');
-        const playerName = assistant.getArgument(PLAYER_NAME_PARAM);
-
-        playersRef.child(playerName).once('value', snap => {
-            var count = (snap.val() || {}).count || 0
-
-            const speech = `<speak>${playerName} lleva ${count} fabas</speak>`;
+        var newMessageRef = spreadsRef.push();
+        newMessageRef.set({
+            user: userName,
+            timestamp: firebase.database.ServerValue.TIMESTAMP,
+            msg: message,
+        }, function onComplete() {
+            const speech = `<speak>TODO ${userName} turtledove sent!</speak>`;
             assistant.ask(speech);
         });
    }
 
-   function removeFaba(assistant) {
-        console.log('removeFaba');
-        const playerName = assistant.getArgument(PLAYER_NAME_PARAM);
+   function readSpread(assistant) {
+        console.log('readSpread');
+        const playerName = assistant.getArgument(USERNAME_PARAM);
 
-        playersRef.child(playerName).once('value', snap => {
+        spreadsRef.child(playerName).once('value', snap => {
             var count = (snap.val() || {}).count || 0
 
-            playersRef.child(playerName).set({
-                count: count - 1
-            });
+            const speech = `<speak>${playerName} lleva ${count} fabas</speak>`;
+            assistant.ask(speech);
         });
    }
 });
